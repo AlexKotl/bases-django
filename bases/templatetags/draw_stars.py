8aca8b117705fc002b7d4b69a4f4ea5605147ee8
Alex <slicer256@gmail.com>
2018-07-06 15:12:05 +0300

    draw stars template filter

--- bases/templatetags/draw_stars.py
+++ bases/templatetags/draw_stars.py
@@ -3,5 +3,19 @@ from django import template
 register = template.Library()
 
 @register.filter
-def draw_stars(value):
-    return "xxxxx"
+def draw_stars(rating):
+    html = '';
+
+    if rating == 0:
+        return ''
+
+    for i in range(1, 6):
+        if rating < i:
+            if rating >= i - 0.5:
+                star = 'half'
+            else:
+                star = 'off'
+        else:
+            star = 'on'
+        html += "<span class='icon star {star}' alt='' title='{rating}'></span>".format(star=star, rating=round(rating,1))
+    return html
