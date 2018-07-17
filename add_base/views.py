b563bbc69e2fe6421d3f19a58c0bade088492c78
Alex <slicer256@gmail.com>
2018-07-17 19:17:24 +0300

    small changes on add form

--- add_base/views.py
+++ add_base/views.py
@@ -4,8 +4,15 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
+def process_images(request):
+    for file in request.FILES:
+        print("Uploaded file: ")
+        print(file.name)
+    #print(request.FILES.get(0))
+
 def add_submit(request):
-    return HttpResponseRedirect(reverse('add_done'))
+    process_images(request)
+    #return HttpResponseRedirect(reverse('add_done'))
 
 def add_done(request):
     return render(request, 'bases/add_done.html', {})
