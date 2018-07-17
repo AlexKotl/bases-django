cd455a4d33240c6b8abd556df7dc2e54adb08055
Alex <slicer256@gmail.com>
2018-07-17 21:45:57 +0300

    add form converted using standard forms class

--- add_base/form.py
+++ add_base/form.py
@@ -1,4 +1,12 @@
 from django import forms
 
 class AddForm(forms.Form):
+    address = forms.CharField(label='Address')
+    contacts = forms.CharField(widget=forms.Textarea)
+    #pos_x = forms.CharField()
+    #pos_y = forms.CharField()
     name = forms.CharField(label='Base name', max_length=100)
+    price = forms.DecimalField()
+    description = forms.CharField(widget=forms.Textarea)
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
