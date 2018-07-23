005f15557e0bf151fdb0b909644c7674618d13b3
Alex <slicer256@gmail.com>
2018-07-23 22:26:33 +0300

    added blank auth action to cms

--- control/urls.py
+++ control/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.list, name='list'),
     path('login', views.login, name='login'),
+    path('auth', views.auth, name='auth'),
     #path('/submit', views.add_submit, name='add_submit'),
 ]
--- control/views.py
+++ control/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from bases.models import Base, Comments
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from bases.settings_private import *
 
 def list(request):
     return render(request, 'control/list.html', {})
 
 def login(request):
     return render(request, 'control/login.html')
+
+def auth(request):
+    return HttpResponseRedirect(reverse('list'))
