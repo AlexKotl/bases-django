4fb4ff2f4ae60f72b36f7015fd07da8114a35289
Alex <slicer256@gmail.com>
2018-07-23 22:47:24 +0300

    basic session action

--- control/views.py
+++ control/views.py
@@ -5,10 +5,14 @@ from django.urls import reverse
 from bases.settings_private import *
 
 def list(request):
+    if not 'access_level' in request.session:
+        return HttpResponseRedirect(reverse('login'))
     return render(request, 'control/list.html', {})
 
 def login(request):
     return render(request, 'control/login.html')
 
 def auth(request):
+    if True:
+        request.session['access_level'] = 1
     return HttpResponseRedirect(reverse('list'))
